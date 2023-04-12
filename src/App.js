4cd6a5fa2e85075a354b3c8b100cc06fb9a24f67
Nasikh CL <nasikcl@gmail.com>
2023-04-12 11:36:00 +0530

    chat application mapped datas to chat list / created intrConvo / working on conversation component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import ChatList from './components/ChatList';
 import Conversation from './components/Conversation';
+import {datas} from './datas'
+import ConvoIntro from './components/ConvoIntro';
 
+ 
 function App() {
+  const [messageDatas, setMessageDatas] = useState({...datas})
+  const[currentConversation, setCurrentConversation] = useState({})
+
+  const setCurrentConvo = (id)=>{
+    const currentChat = messageDatas.friends.find(friend=> friend.id === id)
+    setCurrentConversation(currentChat)
+  }
+  // console.log(datas)
+    
+  
+  // useEffect(()=>{
+  //   setMessageDatas(datas)
+
+    
+
+  // },[])
+  console.log(currentConversation.length)
   return (
     <div className="App">
-      <div class="whatsapp-background-top"></div>
-      <div class="whatsapp-background-bottom"></div>
-      <ChatList />
-      <Conversation />
+      <div className="whatsapp-background-top"></div>
+      <div className="whatsapp-background-bottom"></div>
+      <ChatList profileData={messageDatas.profile} friendsArr={messageDatas.profile.friends} passSetCurrentConvo={setCurrentConvo}/>
+      {
+       Object.keys(currentConversation).length === 0 ?   
+        <ConvoIntro /> :
+       <Conversation convoDetails={currentConversation} /> 
+      }
     </div>
   );
 }
--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -2,30 +2,31 @@ import React from 'react'
 import '../index.css'
 import FriendCard from './FriendCard'
 
-const ChatList = () => {
+const ChatList = ({profileData, friendsArr, passSetCurrentConvo}) => {
+    console.log(profileData.picture)
     return (
-        <section class="chat-list-container">
-        <div class="user-settings">
-            <div class="user-profile-picture">
-                <img src="https://media.licdn.com/dms/image/D5603AQFelednnKJlJg/profile-displayphoto-shrink_400_400/0/1667584272134?e=1682553600&v=beta&t=ZpNiI5qmaz39IMDdsUNeoGz84GBUsa7Hegwbsx5WX_Q" alt="" />
+        <section className="chat-list-container">
+        <div className="user-settings">
+            <div className="user-profile-picture">
+                <img src={profileData.picture} alt="" /> 
             </div>
-            <div class="settings">
+            <div style={{fontWeight:'600'}}>{profileData.name}</div>
+            <div className="settings">
                
-                <img class="m-2" src="https://img.icons8.com/ios/25/null/loading.png"/>
-                <img class="m-2" src="https://img.icons8.com/ios-filled/25/null/sms.png"/>
-                <img class="m-2" src="https://img.icons8.com/ios-filled/20/null/menu-2.png"/>
+                <img className="m-2" src="https://img.icons8.com/ios/25/null/loading.png"/>
+                <img className="m-2" src="https://img.icons8.com/ios-filled/25/null/sms.png"/>
+                <img className="m-2" src="https://img.icons8.com/ios-filled/20/null/menu-2.png"/>
             </div>
         </div>
-        <div class="search-section">
-            <img class="search-button" src="https://img.icons8.com/ios-glyphs/17/null/search--v1.png" />
-            <input class="search-box" type="text" placeholder="Search or start new chat" />
+        <div className="search-section">
+            <img className="search-button" src="https://img.icons8.com/ios-glyphs/17/null/search--v1.png" />
+            <input className="search-box" type="text" placeholder="Search or start new chat" />
         </div>
 
-        <div class="chat-list">
-           <FriendCard /> 
-           <FriendCard /> 
-           <FriendCard /> 
-           <FriendCard /> 
+        <div className="chat-list">
+           {friendsArr.map(friend=>{
+            return(<FriendCard friendData={friend} passSetCurrentConvo={passSetCurrentConvo}/>)
+           })}
          
         </div>
     </section>
